e351881edb422a6da6aa7dec0bbbeb2fe7b5b49c
Saurabh <saurabhmishraald03@gmail.com>
2025-04-06 17:17:51 +0530

    Home Page with basic features and

--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -1,6 +1,25 @@
+import AnimationWrapper from "../common/page-animation"
+import InPageNavigation from "../components/inpage-navigation.component"
 const HomePage = () => {
     return (
-        <h1>Home page coming from componet jsx</h1>
+        <AnimationWrapper>
+            <section className="h-cover flex justify-center gap-10">
+                {/* {Latest Blogs} */}
+                <div className="w-full">
+
+                    <InPageNavigation routes={["Home", "Trending Blogs"]} defaulthidden = {["Trending Blogs"]}>
+                        <h1>Latest Blogs Here</h1>
+                        <h1>Trending Blogs Here</h1>
+                    </InPageNavigation>
+
+                </div>
+                {/* {filters and trending blogs } */}
+                <div>
+
+                </div>
+
+            </section>
+        </AnimationWrapper>
     )
 }
 
