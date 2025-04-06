ef43e289f1654413bc467db1c0efb79f4192219d
Saurabh <saurabhmishraald03@gmail.com>
2025-04-06 22:07:11 +0530

    Added Home page with two section for showing latest blogs

--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -1,6 +1,29 @@
+import axios from "axios"
 import AnimationWrapper from "../common/page-animation"
 import InPageNavigation from "../components/inpage-navigation.component"
+import { useEffect, useState } from "react"
+import Loader from "../components/loader.component"
+import BlogPostCard from "../components/blog-post.component"
 const HomePage = () => {
+
+    let [blogs, setBlog] = useState(null)
+
+
+    const fetchlatestblogs = () => {
+        axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
+        .then(({data})=>{
+            // console.log(data.blogs)
+            setBlog(data.blogs)
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
+
+    useEffect(()=>{
+        fetchlatestblogs();
+    },[])
+
     return (
         <AnimationWrapper>
             <section className="h-cover flex justify-center gap-10">
@@ -8,7 +31,19 @@ const HomePage = () => {
                 <div className="w-full">
 
                     <InPageNavigation routes={["Home", "Trending Blogs"]} defaulthidden = {["Trending Blogs"]}>
-                        <h1>Latest Blogs Here</h1>
+
+                        <>
+                        {
+                            blogs ==null ? <Loader />:
+                            blogs.map((blog,i) =>{
+                                return <AnimationWrapper transition={{duration:1, delay: i*.1}} key={i}>
+                                    <BlogPostCard content={blog} 
+                                    author={blog.author.personal_info} />
+                                </AnimationWrapper>
+                            })
+                        }
+                        </>
+
                         <h1>Trending Blogs Here</h1>
                     </InPageNavigation>
 
